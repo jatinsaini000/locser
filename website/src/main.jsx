dc7c453658ef23d0ca9642df23cb8fccfc631e94
Jatin <sainijatin505@gmail.com>
2026-05-15 19:40:45 +0530

    Fixed Google Login client_id missing in production build

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import 'leaflet/dist/leaflet.css'
 import App from './App.jsx'
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+const GOOGLE_CLIENT_ID = '755741213878-frmf2hs7o2muvrdejao2m7kethe5qabj.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
